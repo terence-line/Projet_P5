9e8bce89ad2b5bf458d3ad2af23f083abfe7f124
terence-line <terence_line@hotmail.fr>
2022-07-30 17:52:15 +0200

    script product.js avec infos produits

--- front/js/product.js
+++ front/js/product.js
@@ -1,15 +1,75 @@
-/*console.log(window.location);
+// console.log(window.location.search);
+console.log(window.location);
+
 
 // création nouvel url avec urlSearchParams
+// let params = new URLSearchParams(document.location.search);
+// let id = params.get('id');
+let str = window.location.href;
+let url = new URL(str);
+let params = new URLSearchParams(document.location.href)
+let id = url.searchParams.get("id");
+console.log('id');
 
-let params = new URLSearchParams(document.location.search);
-let id = params.get("id");
 
+// Creation des variables. Elle sont déclarées en dehors pour pouvoir les appelées plus tard si besoin.
+
+let elementImage = document.querySelector(".item__img");
+let elementName = document.querySelector("#title");
+let elementPrice = document.querySelector("#price");
+let elementDescription = document.querySelector("#description");
+let elementColors = document.querySelector("#colors");
 
 
 // création nouvelle adresse fetch + identifiant produit
 
 fetch(`http://localhost:3000/api/products/${id}`)
+
+    .then(function (response) {
+        console.log(response);
+        // Utilisation d'une condition pour l'affichage de la reponse dans la console.
+        if (response.ok) {
+            return response.json();
+        }
+    })
+
+    .then (function (result) {
+        // Affichage du produit dans la console.
+        console.log(result);
+
+        // insertion image
+        const productImg = document.createElement("img");
+        productImg.setAttribute("src", result.imageUrl);
+        productImg.setAttribute("alt", result.altTxt);
+        elementImage.appendChild(productImg);
+        
+        // insertion nom
+        elementName.innerHTML = result.name;
+
+        // insertion prix            
+        elementPrice.innerHTML = result.price;
+        
+        // insertion decxription           
+        elementDescription.innerHTML = result.description;
+    
+        // insertion couleurs
+        let productColors = result.colors;
+        let monHtml = "";
+        for (let i= 0; i < productColors.length; i++) {
+            console.log(productColors[i]);
+            monHtml += '<option value = "' + productColors[i]+ '">' + productColors[i]+"</option>";
+        }
+            console.log(monHtml);
+            document.querySelector("select").innerHTML += monHtml;
+                    
+        
+        });
+
+    
+
+        // création nouvelle adresse fetch + identifiant produit
+
+/*fetch(`http://localhost:3000/api/products/${id}`)
     .then((response) => {
         return response.json();   
         console.log(response);
@@ -26,62 +86,24 @@ fetch(`http://localhost:3000/api/products/${id}`)
         document.querySelector(".item__img").setAttribute("src", result.imageUrl);
 
         // insertion nom
-
-        const productName = document.createElement("title");
-
-        productName.setAttribute("name", result.name);
-
-        document.querySelector("#title").setAttribute("name", result.name);
-
-        console.log(result);
-    });*/
-
-console.log(window.location);
-
-// Creation des variables. Elle sont déclarées en dehors pour pouvoir les appelées plus tard si besoin.
-
-let elementImage = document.querySelector(".item__img");
-let elementName = document.querySelector("#title");
-let elementPrice = document.querySelector("#price");
-let elementDescription = document.querySelector("#description");
-
-// création nouvel url avec urlSearchParams
-
-let params = new URL(document.location).searchParams;
-let id = params.get("id");
-console.log(id);
-
-
-
-fetch(`http://localhost:3000/api/products/${id}`)
-    .then(function (response) {
-        console.log(response);
-        // Utilisation d'une condition pour l'affichage de la reponse dans la console.
-        if (response.ok) {
-            return response.json();
-        }
-    })
-
-    .then(function (result) {
-        // Affichage du produit voulu dans la console.
-        console.log(result);
-
-        // Placement des donnees API aux bons endroits. 
-
-        // insertion image
-        const productImg = document.createElement("img");
-        productImg.setAttribute("src", result.imageUrl);
-        productImg.setAttribute("alt", result.altTxt);
-        elementImage.appendChild(productImg);
-        // document.querySelector('.item_img').setAttribute("src", result.imageUrl);
-        console.log(productImg);
-
-        // insertion nom
-        elementName.textContent = result.name;
+        elementName.innerHTML = result.name;
 
         // insertion prix            
-        elementPrice.textContent = result.price;
+        elementPrice.innerHTML = result.price;
         
         // insertion decxription           
-        elementDescription.textContent = result.description;
-    });
+        elementDescription.innerHTML = result.description;
+    
+        // insertion couleurs
+        let productColors = result.colors;
+        let monHtml = "";
+        for (let i=0; i<productColors.length; i++) {
+            console.log(productColors[i]);
+            monHtml += '<option value = "' + productColors[i]+ '">' + productColors[i]+"</option>";
+        }
+            console.log(monHtml);
+            document.querySelector("select").innerHTML += monHtml;
+                       
+    });*/
+
+
