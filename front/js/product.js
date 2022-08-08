50e960429dc887aa9716553fcf7114b4bb471fa6
terence-line <terence_line@hotmail.fr>
2022-08-08 22:52:28 +0200

    ajout message d'alerte

--- front/js/product.js
+++ front/js/product.js
@@ -1,7 +1,8 @@
 let myId = new URL(document.location.href).searchParams.get("id")
-//priceProduct = [];
 
-// Creation des variables. Elle sont déclarées en dehors pour pouvoir les appelées plus tard si besoin.
+
+// Creation des variables. 
+// Elles sont déclarées en dehors pour pouvoir les appeler plus tard si besoin.
 
 // let elementImage = document.querySelector(".item__img");
 let elementName = document.querySelector("#title");
@@ -13,51 +14,49 @@ afficher_produit();
 
 // création nouvelle adresse fetch + identifiant produit
 function afficher_produit() {
-    fetch('http://localhost:3000/api/products/'+ myId)
-    .then((response) => {    
-         return response.json();
-        }
-    )
-    .then ((resultApi) => {
-        // Affichage du produit dans la console.
-        //console.table(resultApi);
+    fetch('http://localhost:3000/api/products/' + myId)
+        .then((response) => {
+            return response.json();
+        })
+        .then((resultApi) => {
+            // Affichage du produit dans la console.
+            //console.table(resultApi);
 
-        let elementImage = document.querySelector(".item__img");
+            let elementImage = document.querySelector(".item__img");
 
-        // insertion image
-        const productImg = document.createElement("img");
-        productImg.setAttribute("src", resultApi.imageUrl);
-        productImg.setAttribute("alt", resultApi.altTxt);
-        elementImage.appendChild(productImg);
-            
-        // insertion nom
-        elementName.innerHTML = resultApi.name;
+            // insertion image
+            const productImg = document.createElement("img");
+            productImg.setAttribute("src", resultApi.imageUrl);
+            productImg.setAttribute("alt", resultApi.altTxt);
+            elementImage.appendChild(productImg);
 
-        // insertion prix            
-        elementPrice.innerHTML = resultApi.price;
-        
-            
-        // insertion decxription           
-        elementDescription.innerHTML = resultApi.description;
-        
-        // insertion couleurs
-        let productColors = resultApi.colors;
-        let monHtml = "";
-        for (let i= 0; i < productColors.length; i++) {
-            monHtml += '<option value = "' + productColors[i]+ '">' + productColors[i]+"</option>";
-        }
+            // insertion nom
+            elementName.innerHTML = resultApi.name;
 
-        document.querySelector("select").innerHTML += monHtml;    
-        
-        ajouter_panier(resultApi);
-        
-    })
+            // insertion prix            
+            elementPrice.innerHTML = resultApi.price;
 
-    .catch ((err) => {
-        console.log(err);
-    })
+
+            // insertion decxription           
+            elementDescription.innerHTML = resultApi.description;
+
+            // insertion couleurs
+            let productColors = resultApi.colors;
+            let monHtml = "";
+            for (let i = 0; i < productColors.length; i++) {
+                monHtml += '<option value = "' + productColors[i] + '">' + productColors[i] + "</option>";
+            }
+
+            document.querySelector("select").innerHTML += monHtml;
+
+            ajouter_panier(resultApi);
+
+        })
+
+        .catch((err) => {
+            console.log(err);
+        })
 }
-
 
 // .......  Local storage .............
 
@@ -66,17 +65,17 @@ function afficher_produit() {
 // JSON.parse() => La méthode JSON.parse() analyse une chaîne de caractères JSON et construit la valeur JavaScript ou l'objet décrit par cette chaîne.
 // parseInt() => La fonction parseInt() analyse une chaîne de caractère fournie en argument et renvoie un entier exprimé dans une base donnée.
 
-function ajouter_panier(product) {
-    let tableauLocalStorage = JSON.parse(localStorage.getItem("productId"));
-    let myColor = document.querySelector("#colors").value;
-    let myQuantity = document.querySelector("#quantity");
+function ajouter_panier() {
+    const tableauLocalStorage = JSON.parse(localStorage.getItem("productId"));
+    const myColor = document.querySelector("#colors").value;
+    const myQuantity = document.querySelector("#quantity");
 
-    
+
 
     // Création  d'un tableau de données (product)
-     addToCart.onclick = () => {
-
-        let myColor = document.querySelector("#colors");
+    addToCart.onclick = () => {
+       
+        /*let myColor = document.querySelector("#colors");
 
         let product = {
             id: myId,
@@ -84,19 +83,27 @@ function ajouter_panier(product) {
             quantity: myQuantity.value,
             quantity: parseInt(myQuantity.value)
         }
-        console.log(product);
+        console.log(product);*/
 
-
-       if(tableauLocalStorage == null) {
-            // Ajouter un produit au localStorage 
-            localStorage.setItem("product", JSON.stringify(product));     
+        if(myQuantity.value == 0 || myColor != "") {
+            alert("Veuillez sélectionner une couleur et une quantité.")
+            
         }
+        else {
+
+            let myColor = document.querySelector("#colors");
+
+            let product = {
+                id: myId,
+                color: myColor.value,
+                quantity: myQuantity.value,
+                quantity: parseInt(myQuantity.value)
+            }
+            console.log(product);
         
-        
-        //Alimenter le tableau des prix 
-        // Tableau des prix : (ne jamais stocker les prix dans le localStorage) 
-        //priceProduct.push(product);
-   
+            // Ajouter un produit au localStorage 
+            localStorage.setItem("productId", JSON.stringify(product))
+        }
     }
-}  
-   
+}
+
