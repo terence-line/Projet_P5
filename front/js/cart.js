cc803fe32c3af9e3aa5e68f630387ca1fcdb6115
terence-line <terence_line@hotmail.fr>
2022-08-10 19:33:22 +0200

    carts.js_ ajout html dans le DOM_premier test

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,2 +1,64 @@
 // Importation du local storage de la page produit
 const tableauLocalStorage = JSON.parse(localStorage.getItem("products"));
+console.log(tableauLocalStorage);
+let myId = new URL(document.location.href).searchParams.get("id")
+prixProduits = [];
+
+
+
+
+// Affichage des produits du local storage
+const kanapPanier = document.querySelector("#cart__items");
+console.log(kanapPanier);
+
+// Vérification du contenu du panier : si le panier est vide
+if (tableauLocalStorage === null){
+    alert("panier vide");
+}
+// Si le panier est plein, afficher les produits
+else {
+    let panierPlein = [];
+    
+    for(i=0; i < tableauLocalStorage.length; i++) {
+        //console.log(tableauLocalStorage.length);
+        panierPlein = panierPlein + 
+
+        `<article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+            <div class="cart__item__img">
+                <img src="${tableauLocalStorage[i].imageUrl}" alt="${tableauLocalStorage[i].altTxt}">
+            </div>
+            <div class="cart__item__content">
+                <div class="cart__item__content__description">
+                    <h2>${tableauLocalStorage[i].name}</h2>
+                    <p>${tableauLocalStorage[i].myColorValue}</p>
+                    <p>${tableauLocalStorage[i].price} €</p>
+                </div>
+                <div class="cart__item__content__settings">
+                    <div class="cart__item__content__settings__quantity">
+                        <p>Qté : ${tableauLocalStorage[i].quantity}</p>
+                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value=" ${tableauLocalStorage[i].quantity}">
+                    </div>
+                    <div class="cart__item__content__settings__delete">
+                        <p class="deleteItem">Supprimer</p>
+                    </div>
+                </div>
+            </div>
+        </article>`;
+        }
+
+    kanapPanier.innerHTML = panierPlein;
+   
+
+
+    // Alimenter le tableau des prix
+    prixProduits.push("products");
+    console.log(prixProduits);
+
+}
+     
+           
+                  
+        
+        
+           
+               
